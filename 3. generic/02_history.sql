b6d577000b4af4b9b5fa9fbddceed9e79d043e3b
João Ribeiro <joaosoft@gmail.com>
2019-01-15 20:44:39 +0000

    first commit

--- 3. generic/02_history.sql
+++ 3. generic/02_history.sql
@@ -10,49 +10,67 @@ CREATE TABLE history.example_one (
   LIKE model.example_one
 );
 
-CREATE OR REPLACE FUNCTION create_insert_history_statement(_schemaname text, _tablename text, _row anyelement, _operation history.operation) RETURNS text
+CREATE OR REPLACE FUNCTION create_insert_history_statement(_history_schema_name text, _history_table_name text, _model_schema_name text, _model_table_name text, _row anyelement, _operation history.operation) RETURNS text
     LANGUAGE plpgsql
     AS $$
 DECLARE
-  rec RECORD;
-  _value text;
-  _columns text[];
-  _values text[];
+	_rec RECORD;
+	_value text;
+	_columns text[];
+	_values text[];
 BEGIN
-FOR rec IN
+FOR _rec IN
     SELECT column_name
     FROM information_schema.columns
     WHERE
-        table_schema = quote_ident(_schemaname)
-    AND table_name = quote_ident(_tablename)
+        table_schema = quote_ident(_model_schema_name)
+    AND table_name = quote_ident(_model_table_name)
     ORDER BY ordinal_position
 LOOP
-	IF substring(rec.column_name, 1, 1) <> '_' THEN
-		EXECUTE 'SELECT ($1).' || rec.column_name INTO STRICT _value USING _row;
-	    _columns := _columns || quote_ident(rec.column_name::text);
+	IF substring(_rec.column_name, 1, 1) <> '_' THEN
+		EXECUTE 'SELECT ($1).' || _rec.column_name INTO STRICT _value USING _row;
+	    _columns := _columns || quote_ident(_rec.column_name::text);
 	    _values := _values || format('%L', _value);
 	END IF;
 END LOOP;
-RETURN format('INSERT INTO history.%s (_user, _operation, _operation_at, %s) VALUES (%L, %L, %L, %s)', _tablename, array_to_string(_columns, ','), user, _operation, now(), array_to_string(_values, ','));
+RETURN format('INSERT INTO %s.%s (_user, _operation, _operation_at, %s) VALUES (%L, %L, %L, %s)', _history_schema_name, _history_table_name, array_to_string(_columns, ','), user, _operation, now(), array_to_string(_values, ','));
 END;
 $$;
 
 CREATE OR REPLACE FUNCTION function_insert_history() RETURNS TRIGGER AS $$
-begin
-    IF (TG_OP = 'INSERT') then
-    	--RAISE EXCEPTION 'Testing [%!]', create_insert_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, OLD);
-        EXECUTE(create_insert_history_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW, 'I'));
+DECLARE
+	_err_context text;
+	_history_schema_name text;
+	_history_table_name text;
+	_model_schema_name text;
+	_model_table_name text;
+BEGIN
+    _history_schema_name := 'history';
+   	_history_table_name := TG_TABLE_NAME;
+  	_model_schema_name := TG_TABLE_SCHEMA;
+ 	_model_table_name := TG_TABLE_NAME;
+	IF (TG_OP = 'INSERT') then
+    	--RAISE EXCEPTION 'Testing [%!]', create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, NEW, 'I'));
+        EXECUTE(create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, NEW, 'I'));
         RETURN NEW;
     ELSIF (TG_OP = 'UPDATE') then
       -- catch errors
-    	--RAISE EXCEPTION 'Testing [%!]', create_insert_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW);
-        EXECUTE(create_insert_history_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW, 'U'));
+    	--RAISE EXCEPTION 'Testing [%!]', create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, NEW, 'U');
+        EXECUTE(create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, NEW, 'U'));
         RETURN NEW;
     ELSIF (TG_OP = 'DELETE') then
-    	--RAISE EXCEPTION 'Testing [%!]', create_insert_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW);
-		EXECUTE(create_insert_history_statement(TG_TABLE_SCHEMA, TG_TABLE_NAME, OLD, 'D'));
+    	--RAISE EXCEPTION 'Testing [%!]', create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, OLD, 'D');
+        EXECUTE(create_insert_history_statement(_history_schema_name, _history_table_name, _model_schema_name, _model_table_name, OLD, 'D'));
         RETURN OLD;
     END IF;
+	EXCEPTION
+	WHEN OTHERS THEN
+	    GET STACKED DIAGNOSTICS _err_context = PG_EXCEPTION_CONTEXT;
+		RAISE EXCEPTION 'Error context: %; Error name: %; Error state: %', _err_context, SQLERRM, sqlstate
+		USING HINT = format('Check history table [%s.%s] of table [%s.%s]', _history_schema_name, _history_table_name, _model_schema_name, _model_table_name),
+		SCHEMA = _history_schema_name,
+		TABLE = _history_table_name;
+
 END;
 $$ LANGUAGE plpgsql;
 
